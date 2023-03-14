2f1a583819016254ad6ffb615c6d93ec72bc1190
Joe Tsindos <jtsindos@gmail.com>
2023-03-14 20:43:31 +1100

    init

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Button, StyleSheet, View, ActivityIndicator } from 'react-native'
+import * as ImagePicker from 'expo-image-picker'
+import { useState } from 'react'
 
-export default function App() {
+export default function App () {
+  const [isLoading, setIsLoading] = useState()
+
+  const chooseVideo = async () => {
+    // we can use the following if there are issues with permissions
+    // see https://docs.expo.dev/versions/latest/sdk/imagepicker/#usemedialibrarypermissionsoptions
+
+    const permissions = await ImagePicker.getMediaLibraryPermissionsAsync()
+    if (permissions && !permissions.granted) {
+      await ImagePicker.requestMediaLibraryPermissionsAsync()
+    }
+
+    setIsLoading(true)
+
+    await ImagePicker.launchImageLibraryAsync({
+      quality: 0.2,
+      base64: true,
+      mediaTypes: ImagePicker.MediaTypeOptions.Videos
+    })
+
+    setIsLoading(false)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title='Choose video' onPress={chooseVideo} />
+      {isLoading ? <ActivityIndicator /> : <></>}
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +39,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
